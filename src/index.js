e92cfc944b2f44f9ae57582e741f6014c018330e
Marie Lynn <lynn.topher@gmail.com>
2017-09-08 15:08:24 -0400

    initial perch animation

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import SVGInjector from 'svg-injector';
 $(document).ready(function() {
   console.log("Document ready to go!");
 
+
+
+
   // Elements to inject
   var mySVGsToInject = document.querySelectorAll('img.inject-me');
 
@@ -31,56 +34,14 @@ $(document).ready(function() {
 
 }); //end Document ready
 
-$("#explode").click(function() {
-  var paths = $("path").each(function() {
-    console.log(this.id); //print out all the path ids
-  });
 
-  TweenMax.staggerTo(shuffle(paths), 0.1, {
-    cycle: {
-      x: [100, -100],
-      rotation: [30, 60, 90]
-    },
-    repeat: 0
-  }, 0.1);
+
+$("#swim").click(function() {
+  var tl = new TimelineLite();
+  tl.staggerTo("#fish", 1, {rotation:360, x:100}, 0.5);
+
+      tl.play();
+
+      
 
 });
-
-$("#walk").click(function() {
-  var l1 = new TimelineMax();
-  l1.to("#g75743", 1, {
-    x: -10,
-    y: 10,
-  }, "left");
-
-  l1.to('#g75743', 1, {
-    rotation: "-=5",
-    ease: Linear.easeNone
-  });
-  l1.to("#g75743", 1, {
-    x: -20,
-    y: 20,
-  }, "left");
-});
-
-
-// Util function to shuffle an array.
-function shuffle(array) {
-  var currentIndex = array.length,
-    temporaryValue, randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}
